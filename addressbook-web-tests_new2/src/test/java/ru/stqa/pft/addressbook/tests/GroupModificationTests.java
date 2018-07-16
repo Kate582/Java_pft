f46ab228ad57c7804af773894e1a2e90e4f06bb5
Kate582 <40356227+Kate582@users.noreply.github.com>
2018-07-16 23:40:09 +0500

    new test without recording

---
+++ addressbook-web-tests_new2/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,20 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.GroupData;
+
+/**
+ * Created by Екатерина on 7/16/2018.
+ */
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().gotoGroupPage();
+    app.getGroupHelper().selectGroup();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("name1", "header1", "footer1"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
